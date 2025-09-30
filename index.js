afaf082a2f140ed33e027767f137388b919a94be
tylekmj-ops <tylekmj@gmail.com>
2025-09-30 13:01:33 -0400

    Completed

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ Below 40 → "Wear a heavy coat, scarf, and boots."
 
 const outfitPicker = (temp) => {
   // ✍️ Write your code here
+  if (temp >= 80) {
+    return "Wear shorts and a t-shirt";
+  } else if (temp >= 60) {
+    return "Wear jeans and a light jacket";
+  } else if (temp >= 40) {
+    return "Wear a sweater and pants";
+  } else {
+    return "Wear a heavy coat, scarf, and boots";
+  }
 };
 
 //Test your function below
+console.log(outfitPicker(35))
+console.log(outfitPicker(95))
+console.log(outfitPicker(75))
+console.log(outfitPicker(45))
